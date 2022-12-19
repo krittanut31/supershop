262f5caefb03418a42bf44ce1db1d3cc7ad8493d
krittanut31 <krittanat12300@gmail.com>
2022-12-19 15:36:40 +0700

    add odedr in product page

--- src/page/Uniform.js
+++ src/page/Uniform.js
@@ -1,8 +1,21 @@
+import { uniform } from "../data/Product";
+
 const Uniform = () => {
   return (
-    <div>
+    <div clas>
       <div className=" bg-[url('https://cdn.shopify.com/s/files/1/1864/2187/files/Bite_Holiday_Content1109-16_9.jpg')] bg-cover w-full h-[80rem]">
-        Uniform
+        <div>
+          <p>Uniform</p>
+        </div>
+        <div className="grid grid-cols-3 ">
+          {uniform.map((uniform) => (
+            <div className="">
+              <p>
+                {uniform.name} {uniform.price}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
